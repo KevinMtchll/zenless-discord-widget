05fc9e41ecb2edbdfa7283c3fdd1ed261993d746
Kevin Mitchell <kmitch04@umd.edu>
2026-07-16 18:51:37 -0400

    Test text 6

--- core/status_card.py
+++ core/status_card.py
@@ -22,10 +22,10 @@ FONT_SIZE_SMALL = 24
 TEXT_COLOR = "white"
 
 COORDS: dict[str, tuple[int, int]] = {
-    "text_battery": (70,480),
+    "text_battery": (60,480),
     "battery": (170, 540),
-    "text_engagement": (588,70),
-    "engagement": (1200, 70),
+    "text_engagement": (588,60),
+    "engagement": (1200, 60),
     "weekly_task": (374, 35),
     "hollow_bounty": (541, 35),
     "member_card": (708, 35),
